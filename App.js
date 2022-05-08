b066cdc7051ae97b9c7ee0c340f56c0a44aba613
Ania Kowalska <a.kowalska@qarbon.it>
2022-05-08 17:44:23 +0200

    navigation

--- App.js
+++ App.js
@@ -1,26 +1,16 @@
 import { Provider } from 'react-redux';
-import styled from 'styled-components/native';
-import { Nested } from '@/Nested';
 import { store } from '@/store/store';
 import ThemeProvider from '@/ThemeProvider';
 import StatusBar from '@/StatusBar';
+import { AppNavigation } from '@/navigation/AppNavigation';
 
 export default function App() {
 	return (
 		<Provider store={store}>
 			<ThemeProvider>
-				<StyledView>
-					<Nested />
-					<StatusBar />
-				</StyledView>
+				<StatusBar />
+				<AppNavigation />
 			</ThemeProvider>
 		</Provider>
 	);
 }
-
-const StyledView = styled.View`
-	background-color: ${props => props.theme.background};
-	flex: 1;
-	align-items: center;
-	justify-content: center;
-`;
